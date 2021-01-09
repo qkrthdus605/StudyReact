1ee92454485783e7d6a17bd399a054d67e10c8aa
qkrthdus605 <qkrthdus605@gmail.com>
2021-01-09 23:33:57 +0900

    2021.02.09

---
+++ src/Comment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Comment extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>{'My first Component'}</h1>
+            </div>
+        ) 
+    }
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,32 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+   document.getElementById('root')
 );
+
+//element를 생성하고 ReactDom에 rendering 시키기
+// const element = <h1>Hello, world!</h1>;
+// ReactDom.render(
+//   element,
+//   document.getElementById('root') 
+// );
+
+// function tick(){
+//   const element=(
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
+// }
+// setInterval(tick, 1000); 
+//1초마다 함수 호출되면서 element가 생성되고 새로 rendering
+//state change->Compute Diff->Re-render
+//시간이 바뀌고->다시 계산을 하고->다시 rendering
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
